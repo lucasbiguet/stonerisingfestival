e229bed899c2154506a4f634ff8429d468b2f633
Lucas BIGUET-MERMET <lucas.biguetmermet@gmail.com>
2012-12-17 11:21:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoneRisingFestival::Application.config.secret_token = 'd55d77adbe5721c57cb730f05965d7dbf9d7d7a46b76686f093783bf1cb66a152240385d9fceece5a4d0d7ca82814f2afaa38043a935133d9864d8b717b26d9a'
